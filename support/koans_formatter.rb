f1cee328540f41e25e496aaae423cbed32353a93
Alyssa <alydiaz77@gmail.com>
2012-12-26 15:59:23 -0500

    adding basic path to enlightenment output formatting

--- support/koans_formatter.rb
+++ support/koans_formatter.rb
@@ -1,121 +1,206 @@
 require "rspec/core/formatters/base_formatter"
 
 class KoansFormatter < RSpec::Core::Formatters::BaseFormatter
+
+  PROGRESS_FILE_NAME = '.path_progress'
+
   def initialize(output)
-    #puts "==================="
-    #puts "entering initialize"
     super(output)
     @passed_count = 0
-    #puts "exiting initialize"
-    #puts "==================="
-  end
-
-  def start(example_count)
-    #puts "==================="
-    #puts "entering start"
-    super(example_count)
-    #puts "exiting start"
-    #puts "==================="
-  end
-
-  def example_group_started(example_group)
-    #puts "==================="
-    #puts "entering example_group_started"
-    super(example_group)
-    #puts "exiting example_group_started"
-    #puts "==================="
-  end
-
-  def example_started(example)
-    #puts "==================="
-    #puts "entering example_started"
-    super(example)
-    #puts "exiting example_started"
-    #puts "==================="
+    @observations = []
   end
 
   def example_passed(example)
-    #puts "==================="
-    #puts "entering example_passed"
-    @output.puts green("About#{example.example_group.description}\##{example.description} has expanded your awareness")
     @passed_count += 1
-    #puts "exiting example_passed"
-    #puts "==================="
-  end
-
-  def example_pending(example)
-    #puts "==================="
-    #puts "entering example_pending"
-    super(example)
-    #puts "exiting example_pending"
-    #puts "==================="
+    if @passed_count > progress.last.to_i
+      @observations << green("About#{example.example_group.description}\##{example.description} has expanded your awareness")
+    end
   end
 
   def example_failed(example)
-    super(example)
-    if caller.any? { |method| method.include? "fail_filtered" }
-      puts red("About#{example.example_group.description}\##{example.description} has damaged your karma.")
+    if @failure.nil?
+      @failure = example
+      add_progress(@passed_count)
+      @observations << red("About#{example.example_group.description}\##{example.description} has damaged your karma.")
     end
     exit
   end
 
   def start_dump
-    #puts "==================="
-    #puts "entering start_dump"
-    super
-    output.puts "#{@passed_count} of #{@example_count} completed"
-    #puts "exiting start_dump"
-    #puts "==================="
+    if failed?
+      @observations.each { |c| puts c }
+      encourage
+      guide_through_error
+      a_zenlike_statement
+      show_progress
+    else
+      end_screen
+    end
   end
 
-  def dump_pending
-    #puts "==================="
-    #puts "entering dump_pending"
-    super
-    #puts "exiting dump_pending"
-    #puts "==================="
+  def add_progress(prog)
+    @_contents = nil
+    exists = File.exists?(PROGRESS_FILE_NAME)
+    File.open(PROGRESS_FILE_NAME, 'a+') do |f|
+      f.print "#{',' if exists}#{prog}"
+    end
   end
 
-  def dump_failures
-    #puts "==================="
-    #puts "entering dump_failures"
-    super
-    #puts "exiting dump_failures"
-    #puts "==================="
+  def progress
+    if @_contents.nil?
+      if File.exists?(PROGRESS_FILE_NAME)
+        File.open(PROGRESS_FILE_NAME, 'r') do |f|
+          @_contents = f.read.to_s.gsub(/\s/, '').split(',')
+        end
+      else
+        @_contents = []
+      end
+    end
+    @_contents
   end
 
-  def dump_summary(duration, example_count, failure_count, pending_count)
-    #puts "==================="
-    #puts "entering dump_summary"
-    super(duration, example_count, failure_count, pending_count)
-    #puts "exiting dump_summary"
-    #puts "==================="
+  def failed?
+    !@failure.nil?
   end
 
-  def format_backtrace(backtrace, example)
-    #puts "==================="
-    #puts "entering format_backtrace"
-    super(backtrace, example)
-    #puts "exiting format_backtrace"
-    #puts "==================="
+  def assert_failed?
+    @failure.exception.is_a?(RSpec::Expectations::ExpectationNotMetError)
   end
 
-  def close
-    #puts "==================="
-    #puts "entering close"
-    super
-    #puts "exiting close"
-    #puts "==================="
+  def encourage
+    puts
+    puts "The Master says:"
+    puts cyan("  You have not yet reached enlightenment.")
+    if ((recents = progress.last(5)) && recents.size == 5 && recents.uniq.size == 1)
+      puts cyan("  I sense frustration. Do not be afraid to ask for help.")
+    elsif progress.last(2).size == 2 && progress.last(2).uniq.size == 1
+      puts cyan("  Do not lose hope.")
+    elsif progress.last.to_i > 0
+      puts cyan("  You are progressing. Excellent. #{progress.last} completed.")
+    end
+  end
+
+  def guide_through_error
+    puts
+    puts "The answers you seek..."
+    puts red(indent(@failure.exception.message).join)
+    puts
+    puts "Please meditate on the following code:"
+    if assert_failed?
+      puts embolden_first_line_only(indent(find_interesting_lines(@failure.exception.backtrace)))
+    else
+      puts embolden_first_line_only(indent(@failure.exception.backtrace))
+    end
+    puts
+  end
+
+  def a_zenlike_statement
+    if !failed?
+      zen_statement = "Mountains are again merely mountains"
+    else
+      zen_statement = case (@passed_count % 10)
+                        when 0
+                          "mountains are merely mountains"
+                        when 1, 2
+                          "learn the rules so you know how to break them properly"
+                        when 3, 4
+                          "remember that silence is sometimes the best answer"
+                        when 5, 6
+                          "sleep is the best meditation"
+                        when 7, 8
+                          "when you lose, don't lose the lesson"
+                        else
+                          "things are not what they appear to be: nor are they otherwise"
+                      end
+    end
+    puts green(zen_statement)
+  end
+
+  def show_progress
+    bar_width = 50
+    scale = bar_width.to_f/@example_count
+    print green("your path thus far [")
+    happy_steps = (@passed_count*scale).to_i
+    happy_steps = 1 if happy_steps == 0 && @passed_count > 0
+    print green('.'*happy_steps)
+    if failed?
+      print red('X')
+      print cyan('_'*(bar_width-1-happy_steps))
+    end
+    print green(']')
+    print " #{@passed_count}/#{@example_count}"
+    puts
+  end
+
+  def embolden_first_line_only(text)
+    first_line = true
+    text.collect { |t|
+      if first_line
+        first_line = false
+        red(t)
+      else
+        cyan(t)
+      end
+    }
+  end
+
+  def indent(text)
+    text = text.split(/\n/) if text.is_a?(String)
+    text.collect { |t| "  #{t}" }
+  end
+
+  def find_interesting_lines(backtrace)
+    backtrace.reject { |line|
+      line =~ /rspec-expectations|rspec-core|bin/
+    }
+  end
+
+  def end_screen
+    "JRuby 1.9.x Koans"
+    ruby_version = "(in #{'J' if defined?(JRUBY_VERSION)}Ruby #{defined?(JRUBY_VERSION) ? JRUBY_VERSION : RUBY_VERSION})"
+    ruby_version = ruby_version.side_padding(54)
+    completed = <<-ENDTEXT
+                                  ,,   ,  ,,
+                                :      ::::,    :::,
+                   ,        ,,: :::::::::::::,,  ::::   :  ,
+                 ,       ,,,   ,:::::::::::::::::::,  ,:  ,: ,,
+            :,        ::,  , , :, ,::::::::::::::::::, :::  ,::::
+           :   :    ::,                          ,:::::::: ::, ,::::
+          ,     ,:::::                                  :,:::::::,::::,
+      ,:     , ,:,,:                                       :::::::::::::
+     ::,:   ,,:::,                                           ,::::::::::::,
+    ,:::, :,,:::                                               ::::::::::::,
+   ,::: :::::::,       Mountains are again merely mountains     ,::::::::::::
+   :::,,,::::::                                                   ::::::::::::
+ ,:::::::::::,                                                    ::::::::::::,
+ :::::::::::,                                                     ,::::::::::::
+:::::::::::::                                                     ,::::::::::::
+::::::::::::                      Ruby Koans                       ::::::::::::,
+::::::::::::#{                  ruby_version                     },::::::::::::,
+:::::::::::,                                                      , ::::::::::::
+,:::::::::::::,                brought to you by                 ,,::::::::::::,
+::::::::::::::                                                    ,::::::::::::
+ ::::::::::::::,                                                 ,:::::::::::::
+ ::::::::::::,             EdgeCase Software Artisans           , ::::::::::::
+  :,::::::::: ::::              Ported to rspec by              :::::::::::::
+   ,:::::::::::  ,:                 @alycit                  ,,:::::::::::::,
+     ::::::::::::                                           ,::::::::::::::,
+      :::::::::::::::::,                                  ::::::::::::::::
+       :::::::::::::::::::,                             ::::::::::::::::
+        ::::::::::::::::::::::,                     ,::::,:, , ::::,:::
+          :::::::::::::::::::::::,               ::,: ::,::, ,,: ::::
+              ,::::::::::::::::::::              ::,,  , ,,  ,::::
+                 ,::::::::::::::::              ::,, ,   ,:::,
+                      ,::::                         , ,,
+                                                  ,,,
+    ENDTEXT
+    puts completed
   end
 
   protected
 
   def color(text, color_code)
     color_enabled? ? "#{color_code}#{text}\e[0m" : text
-  end
-
-  def bold(text)
-    color(text, "\e[1m")
   end
 
   def red(text)
@@ -126,34 +211,23 @@ class KoansFormatter < RSpec::Core::Formatters::BaseFormatter
     color(text, "\e[32m")
   end
 
-  def yellow(text)
-    color(text, "\e[33m")
-  end
-
-  def blue(text)
-    color(text, "\e[34m")
-  end
-
-  def magenta(text)
-    color(text, "\e[35m")
-  end
-
   def cyan(text)
     color(text, "\e[36m")
   end
 
-  def white(text)
-    color(text, "\e[37m")
-  end
+end
 
-  def short_padding
-    '  '
+class String
+  def side_padding(width)
+    extra = width - self.size
+    if width < 0
+      self
+    else
+      left_padding = extra / 2
+      right_padding = (extra+1)/2
+      (" " * left_padding) + self + (" " *right_padding)
+    end
   end
-
-  def long_padding
-    '     '
-  end
-
 end
 
 
