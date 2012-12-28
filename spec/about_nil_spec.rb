bb4704d2c412c575ed6fbbca312ad608209437f3
vaughncm <vaughncm@gmail.com>
2012-12-28 14:32:31 -0500

    Adding about_nil_spec.rb

--- spec/about_nil_spec.rb
+++ spec/about_nil_spec.rb
@@ -1,6 +1,23 @@
 describe "Nil" do
 
-  it "is nothing" do
-    1.should == __
+  it "should show that nil is an object" do
+    nil.is_a?(Object).should == __
   end
+
+  it "should not cause null pointer errors when calling methods on nil" do
+    begin
+      nil.some_method_nil_doesnt_know_about
+    rescue Exception => ex
+      ex.class.should == __
+
+      ex.message.should =~ /__/
+    end
+  end
+
+  it "should display methods nil has defined for it" do
+    nil.nil?.should eq __
+    nil.to_s.should eq __
+    nil.inspect.should eq __
+  end
+
 end
